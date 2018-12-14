c3168136a5953cd2c406671b371838ca4a4d172c
Tran <tran.tu@zumepizza.com>
2018-12-14 15:09:44 -0800

    add reactivity & watch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
-import add from './add';
+console.log('starting...');
 
-const obj = {
-  a: 1,
-  b: 2,
-};
+class Vue {
+  constructor(props) {
+    this.$data = props.data;
+    if (props.data) {
+      const { data } = props;
+      console.log('props.data', props.data);
+      Object.keys(data).forEach(key => {
+        let value = data[key];
+        Object.defineProperty(this.$data, key, {
+          get() {
+            return value;
+          },
+          set(val) {
+            const old = value;
+            console.log('SET', key);
+            value = val;
+            if (props.watch[key]) {
+              props.watch[key](val, old);
+            }
+          },
+        });
+      });
+    }
+  }
+}
 
-console.log('Using the spread operator', { ...obj });
-console.log('The result is', add(add(2, 3), 10));
+const v = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    another: 'another',
+  },
+  watch: {
+    message(newVal, old) {
+      console.log('message changed!!', newVal, old);
+    },
+  },
+});
+console.log('GET message', v.$data.message);
+v.$data.message = 'test';
+console.log('GET message 2nd time', v.$data.message);
+
+console.log('GET another', v.$data.another);
+v.$data.another = 'swag';
+console.log('GET another 2nd time', v.$data.another);
